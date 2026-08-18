9c8390c0a03097a14d366fce505d2ac2d333e883
rizkiapriadi48-dev <rizkiapriadi48@gmail.com>
2026-08-18 09:08:30 +0700

    Add files via upload

---
+++ randomReciep.js
@@ -0,0 +1,87 @@
+const recipe = [
+    {
+        id: 1,
+        nama: "Nasi Goreng",
+        bahan: [
+            "1_Nasi",
+            "2_Telur",
+            "3_Bawang Putih",
+            "4_Kecap"
+        ],
+        langkah: [
+            
+         "Panaskan sedikit minyak di wajan", 
+         "Tumis bawang putih, bawang merah, dan cabai merah hingga harum dan matang",
+         "Masukkan nasi putih, aduk rata bersama orak-arik telur dan bumbu tumis",
+         "Tambahkan kecap manis", 
+         "Masukkan irisan daun bawang, aduk sebentar, lalu angkat dan sajikan hangat"
+
+        ]
+    },
+
+    {
+        id: 2,
+        nama: "Indomie Goreng",
+        bahan: [
+            "1_Indomie",
+            "2_Telur",
+            "3_Bawang Merah",
+            "4_Seledri"
+        ],
+        langkah: [
+            "1_Rebus mie dalam air mendidih selama 2 menit hingga setengah matang, lalu tiriskan.", 
+         "2_Panaskan minyak di wajan",
+         "3_Masukkan telur, buat orak-arik hingga matang.",
+         "4_Masukkan sawi hijau, aduk hingga sedikit layu.", 
+         "5_Masukkan mie yang sudah ditiriskan beserta seluruh bumbu Indomie",
+         "6_Aduk cepat hingga semua bumbu dan bahan tercampur rata dan matang. Angkat dan sajikan."
+        ]
+    },
+    
+    {
+        id: 3,
+        nama: "Tempe Mendoan",
+        bahan: [
+            "1 papan tempe",
+            "100 gram tepung terigu",
+            "2 sendok makan tepung beras",
+            "2 batang daun bawang (iris halus)",
+            "Air secukupnya (hingga adonan agak encer)",
+            "Minyak goreng secukupnya"
+        ],
+        langkah: [
+            "1_Campurkan tepung terigu, tepung beras, bumbu halus, dan daun bawang dalam wadah.",
+            "2_Tuang air sedikit demi sedikit sambil diaduk hingga adonan kental encer (khas mendoan).",
+            "3_Celupkan lembaran tempe ke dalam adonan tepung hingga seluruh permukaannya tertutup.",
+            "4_Panaskan minyak dalam jumlah banyak dengan api sedang.",
+            "5_Goreng tempe sebentar saja (sekitar 1-2menit per sisi) hingga adonan matang tetapi tetap lembek dan tidak garing.",
+            "6_Angkat, tiriskan, dan sajikan bersama sambal kecap cabai rawit."
+
+        ]
+    },
+
+    {
+        id: 4,
+        nama: "Soto Lamongan",
+        bahan: [
+            	"1/2 ekor ayam (potong 2 bagian)",
+            	"1.5 liter air",
+            	"2 batang serai (memarkan)",
+            	"3 lembar daun jeruk",
+            	"2 lembar daun salam",
+            	"1 batang daun bawang (iris)",
+            	"Garam, gula, dan kaldu bubuk secukupnya",
+            	"Minyak untuk menumis"
+        ],
+        langkah: [
+            "1_Rebus ayam dalam 1.5 liter air hingga mendidih dan empuk.",
+            "2_Tumis bumbu halus bersama serai, daun jeruk, dan daun salam hingga harum dan matang.",
+            "3_Masukkan tumisan bumbu ke dalam panci rebusan ayam. Tambahkan garam, gula, dan kaldu bubuk.",
+            "4_Angkat ayam dari kuah, lalu goreng sebentar hingga kecokelatan. Suwir-suwir daging ayamnya.",
+            "5_Masukkan irisan daun bawang ke dalam kuah soto, koreksi rasa, lalu matikan api.",
+            "6_Penyajian: Susun soun, kol, ayam suwir, dan telur rebus dalam mangkuk. Tuangkan kuah soto panas, lalu taburi dengan bubuk koya, perasan jeruk nipis, dan sambal."
+        ]
+    }
+    
+]
+console.log(recipe);
