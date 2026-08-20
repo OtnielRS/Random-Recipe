4745ac41dbc023f2b28a6fb26fa5c50cf4302725
Otniel Rahadiyan Sumantri <93241275+OtnielRS@users.noreply.github.com>
2026-08-20 23:16:43 +0700

    Add files via upload

---
+++ script/randomReciep.js
@@ -0,0 +1,183 @@
+const recipe = [
+  {
+    nama: "Nasi Goreng",
+    bahan: ["Nasi", "Telur", "Bawang Putih", "Kecap"],
+    langkah: [
+      "Panaskan sedikit minyak di wajan",
+      "Tumis bawang putih, bawang merah, dan cabai merah hingga harum dan matang",
+      "Masukkan nasi putih, aduk rata bersama orak-arik telur dan bumbu tumis",
+      "Tambahkan kecap manis",
+      "Masukkan irisan daun bawang, aduk sebentar, lalu angkat dan sajikan hangat",
+    ],
+    link: "https://youtu.be/gDKz2o5UuaM?si=iezBwc9hRRMMq4gy",
+    embed: `<iframe width="560" height="315" src="https://www.youtube.com/embed/gDKz2o5UuaM?si=w9sq-Pv8TLrxy8uw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+  },
+
+  {
+    nama: "Indomie Goreng",
+    bahan: ["Indomie", "Telur", "Bawang Merah", "Seledri"],
+    langkah: [
+      "Rebus mie dalam air mendidih selama 2 menit hingga setengah matang, lalu tiriskan.",
+      "Panaskan minyak di wajan",
+      "Masukkan telur, buat orak-arik hingga matang.",
+      "Masukkan sawi hijau, aduk hingga sedikit layu.",
+      "Masukkan mie yang sudah ditiriskan beserta seluruh bumbu Indomie",
+      "Aduk cepat hingga semua bumbu dan bahan tercampur rata dan matang. Angkat dan sajikan.",
+    ],
+    link: "https://youtu.be/o4noixEiH_w?si=dDdevCF95lwWTNPZ",
+    embed: `<iframe width="560" height="315" src="https://www.youtube.com/embed/o4noixEiH_w?si=peYRCQY6otK5YMAO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+  },
+
+  {
+    nama: "Tempe Mendoan",
+    bahan: [
+      "1 papan tempe",
+      "100 gram tepung terigu",
+      "2 sendok makan tepung beras",
+      "2 batang daun bawang (iris halus)",
+      "Air secukupnya (hingga adonan agak encer)",
+      "Minyak goreng secukupnya",
+    ],
+    langkah: [
+      "Campurkan tepung terigu, tepung beras, bumbu halus, dan daun bawang dalam wadah.",
+      "Tuang air sedikit demi sedikit sambil diaduk hingga adonan kental encer (khas mendoan).",
+      "Celupkan lembaran tempe ke dalam adonan tepung hingga seluruh permukaannya tertutup.",
+      "Panaskan minyak dalam jumlah banyak dengan api sedang.",
+      "Goreng tempe sebentar saja (sekitar 1-2menit per sisi) hingga adonan matang tetapi tetap lembek dan tidak garing.",
+      "Angkat, tiriskan, dan sajikan bersama sambal kecap cabai rawit.",
+    ],
+    link: "https://youtu.be/hPKPDZkn-4s?si=X7B5cRBFZ6krbebH",
+    embed: `<iframe width="560" height="315" src="https://www.youtube.com/embed/hPKPDZkn-4s?si=5m73bBukr6wnV871" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+  },
+
+  {
+    nama: "Soto Lamongan",
+    bahan: [
+      "1/2 ekor ayam (potong 2 bagian)",
+      "1.5 liter air",
+      "2 batang serai (memarkan)",
+      "3 lembar daun jeruk",
+      "2 lembar daun salam",
+      "1 batang daun bawang (iris)",
+      "Garam, gula, dan kaldu bubuk secukupnya",
+      "Minyak untuk menumis",
+    ],
+    langkah: [
+      "Rebus ayam dalam 1.5 liter air hingga mendidih dan empuk.",
+      "Tumis bumbu halus bersama serai, daun jeruk, dan daun salam hingga harum dan matang.",
+      "Masukkan tumisan bumbu ke dalam panci rebusan ayam. Tambahkan garam, gula, dan kaldu bubuk.",
+      "Angkat ayam dari kuah, lalu goreng sebentar hingga kecokelatan. Suwir-suwir daging ayamnya.",
+      "Masukkan irisan daun bawang ke dalam kuah soto, koreksi rasa, lalu matikan api.",
+      "Penyajian: Susun soun, kol, ayam suwir, dan telur rebus dalam mangkuk. Tuangkan kuah soto panas, lalu taburi dengan bubuk koya, perasan jeruk nipis, dan sambal.",
+    ],
+    link: "https://youtu.be/WBAYOg9vTAE?si=SntcgVyoPM--hLmu",
+    embed: `<iframe width="560" height="315" src="https://www.youtube.com/embed/WBAYOg9vTAE?si=oGIc-ocGdCF4YNRr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+  },
+  {
+    nama: "Oreg Tempe Kecap",
+    bahan: [
+      "1 papan tempe (potong dadu kecil",
+      "3 sdm kecap manis",
+      "50 ml air",
+      "Minyak goreng secukupnya",
+      "4 butir bawang merah",
+      "2 siung bawang putih",
+      "3 buah cabai hijau besar",
+      "2 buah cabai rawit (opsional)",
+      "1 cm lengkuas (geprek)",
+      "1 lembar daun salam",
+    ],
+    langkah: [
+      "Goreng tempe hingga setengah matang, tiriskan",
+      "Tumis bawang merah dan putih hingga harum",
+      "Masukkan cabai, lengkuas, dan daun salam",
+      "Masukkan tempe goreng ke dalam tumisan",
+      "Tambahkan kecap manis, garam, dan air",
+      "Aduk rata hingga bumbu meresap sempurna",
+      "Masak sampai air menyusut kering habis",
+    ],
+    link: "https://youtu.be/L0IbYvJuZtY?si=06zKlIzk6f3VQXK5",
+    embed: `<iframe width="560" height="315" src="https://www.youtube.com/embed/L0IbYvJuZtY?si=AQSByqq1cl98poSa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+  },
+  {
+    nama: "Telur Dadar Padang",
+    bahan: [
+      "3 butir telur ayam atau bebek",
+      "2 sdm tepung beras",
+      "2 batang daun bawang (iris halus)",
+      "1 lembar daun kunyit (iris halus)",
+      "3 butir bawang merah",
+      "2 siung bawang putih",
+      "3 buah cabai merah keriting",
+      "1/2 sdt ketumbar bubuk",
+      "Garam dan merica secukupnya",
+    ],
+    langkah: [
+      "Kocok lepas telur dalam mangkuk besar",
+      "Masukkan bumbu halus dan tepung beras",
+      "Masukkan daun bawang dan daun kunyit",
+      "Aduk seluruh bahan hingga rata sempurna",
+      "Panaskan minyak agak banyak di wajan",
+      "Tuang adonan telur dengan api kecil",
+      "Balik telur saat bagian bawah kecokelatan",
+      "Masak hingga kedua sisi matang merata",
+    ],
+    link: "https://youtu.be/ccN_rcnHpP4?si=FHUKDrSeOKVWeMJY",
+    embed: `<iframe width="560" height="315" src="https://www.youtube.com/embed/ccN_rcnHpP4?si=8WG8dO1HVG50QhB7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+  },
+  {
+    nama: "Sayur Asem",
+    bahan: [
+      "1 ikat kacang panjang (potong-potong)",
+      "1 buah jagung manis (potong dadu)",
+      "100 gr melinjo dan daunnya",
+      "1 buah labu siam (potong dadu)",
+      "3 buah asam jawa muda1 liter air",
+      "5 butir bawang merah (iris)",
+      "3 siung bawang putih (iris)",
+      "2 buah cabai merah (belah dua)",
+      "2 cm lengkuas (geprek)",
+      "2 lembar daun salam",
+      "Garam dan gula secukupnya",
+    ],
+    langkah: [
+      "Rebus air bersama bumbu iris dan geprek",
+      "Masukkan jagung dan melinjo terlebih dahulu",
+      "Masak hingga jagung mulai melunak matang",
+      "Masukkan labu siam dan asam jawa.",
+      "Masukkan kacang panjang dan daun melinjo",
+      "Tambahkan garam dan gula sesuai selera",
+      "Masak sebentar hingga semua sayur matang",
+    ],
+    link: "https://youtu.be/Ik62jxkjglg?si=nZGZWTICajoZMoyR",
+    embed: `<iframe width="560" height="315" src="https://www.youtube.com/embed/Ik62jxkjglg?si=_oScMqGOrAmZgvDB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+  },
+  {
+    nama: "Tumis Kangkung Terasi",
+    bahan: [
+      "1 ikat kangkung (siangi, cuci bersih)",
+      "1 sdt terasi bakar",
+      "50 ml air",
+      "2 sdm minyak untuk menumis",
+      "4 butir bawang merah",
+      "2 siung bawang putih",
+      "3 buah cabai rawit merah",
+      "1 buah tomat ukuran kecil",
+      "Garam dan gula secukupnya",
+    ],
+    langkah: [
+      "Tumis bawang merah, bawang putih, dan cabai.",
+      "Masukkan terasi bakar, hancurkan hingga harum rata.",
+      "Masukkan potongan tomat, tumis hingga agak layu.",
+      "Masukkan kangkung, besarkan api kompor Anda.",
+      "Tuang air sedikit agar bumbu cepat meresap.",
+      "Tambahkan garam dan gula sesuai selera Anda.",
+      "Aduk cepat hingga kangkung layu matang.",
+      "Angkat segera agar warna tetap hijau segar.",
+    ],
+    link: "https://youtu.be/EVwKGDo6gB4?si=sBiTKjqv6M4ZKlxX",
+    embed: `<iframe width="560" height="315" src="https://www.youtube.com/embed/EVwKGDo6gB4?si=WPiGtgsNNM9Ap-nO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+  },
+];
+
+localStorage.setItem("resep", JSON.stringify(recipe));
